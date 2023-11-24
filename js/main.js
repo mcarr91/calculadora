0c8c0be1dfc81e2d39ac1596426f021dfcf59cc1
Mauricio Carrizo <mauricio.carrizo@itscordoba.edu.ar>
2023-11-24 14:44:04 -0300

    se modificaron los archivos index.html, styles.css y se añadio el archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,71 @@
-const pantalla =document.querySelector(".pantalla");
-const botones = document.querySelectorAll(".btn");
-
+const pantalla = document.querySelector('.pantalla');
+const botones = document.querySelectorAll('.btn');
+const boton_operadores = document.querySelectorAll('.operador');
+const botonC = document.getElementById('c');
+const boton_borrar = document.getElementById('borrar');
+const boton_igual = document.getElementById('igual');
+// -------------------------------------------------------------------------------
+function agregar_numero_pantalla(valor) {
+  if (pantalla.textContent === '0') {
+    pantalla.textContent = '';
+  }
+  pantalla.textContent += valor;
+}
 botones.forEach(boton => {
-    boton.addEventlistener("click", () => {
-        const boton_apretado = boton.textcontent;
-        if (boton.id === "c") {
-            pantalla.textcontent = "0";
-            return;
-        }
-        if (pantalla.textContent === "0") {
-            pantalla.textContent = boton_apretado;
-        }else {
-            pantalla.textContent += boton_apretado;
-        }
+  boton.addEventListener('click', () => {
+    agregar_numero_pantalla(boton.textContent);
+  });
+});
+function agregar_operador(operador) {
+  if (pantalla.textContent !== '0' && !es_operador(pantalla.textContent.slice(-1))) {
+    pantalla.textContent += operador;
+  }
+}
+boton_operadores.forEach(boton => {
+  boton.addEventListener('click', () => {
+    agregar_operador(boton.textContent);
+  });
+});
+// ---------------------------------------------------------------------------------
+function limpiar_pantalla() {
+  pantalla.textContent = '0';
+}
+function borrar_ultimo_caracter() {
+  op = pantalla.textContent.slice(0, -1);
+  pantalla.textContent=op
+}
+function es_operador(caracter) {
+  return caracter === '+' || caracter === '-' || caracter === '*' || caracter === '/';
+}
+botonC.addEventListener('click', limpiar_pantalla);
+boton_borrar.addEventListener('click', borrar_ultimo_caracter);
+boton_igual.addEventListener('click', realizar_calculo);
+// -----------------------------------------------------------------------------------
+function realizar_operacion(operando1, operador, operando2) {
+  switch (operador) {
+    case '+':
+      return operando1 + operando2;
+    case '-':
+      return operando1 - operando2;
+    case '*':
+      return operando1 * operando2;
+    case '/':
+      return operando1 / operando2;
+    default:
+      return 0;
+  }
+}
+function realizar_calculo() {
+  const expresion = pantalla.textContent;
+  const operadores = expresion.match(/[+\-*/]/g);
+  const numeros = expresion.split(/[+\-*/]/g).map(parseFloat);
 
-    })
-
-
-})
+  let resultado = numeros[0];
+  for (let i = 0; i < operadores.length; i++) {
+    const operador = operadores[i];
+    const numero = numeros[i + 1];
+    resultado = realizar_operacion(resultado, operador, numero);
+  }
+  pantalla.textContent = resultado;
+}
+// -----------------------------------------------------------------------------------
